56b76b44bff48d632174ce63b2a833ae5a824e7c
Mar <101468799+MarNuez@users.noreply.github.com>
2022-08-04 00:43:53 -0300

    bien

--- argpro/src/app/app.module.ts
+++ argpro/src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LogoAPComponent,
-    SocialComponent
+ 
   ],
   imports: [
     BrowserModule,
