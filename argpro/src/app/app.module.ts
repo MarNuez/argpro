696abfc2fff89fda0f1e253085291cd799bef7e4
Mar <101468799+MarNuez@users.noreply.github.com>
2022-08-08 19:05:21 -0300

    Revert "educacion"
    This reverts commit 715aac7d7e5fa283a844024f6eb88ab74281a198.

--- argpro/src/app/app.module.ts
+++ argpro/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { SocalComponent } from './components/socal/socal.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
-import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +19,6 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent,
  
   ],
   imports: [
