0d1e25d945f549b4714b0d09f2a8ab4d407ac31d
Mar <101468799+MarNuez@users.noreply.github.com>
2022-08-08 18:35:55 -0300

    experiencialaboral

--- argpro/src/app/app.module.ts
+++ argpro/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocalComponent } from './components/socal/socal.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     SocalComponent,
     BannerComponent,
     AcercaDeComponent,
+    ExperienciaComponent,
  
   ],
   imports: [
