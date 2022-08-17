7b9f617b5fae53200cf49db430d7b01d7dc0ffe5
marnuez <marielanovelli@gmail.com>
2022-08-17 16:34:11 -0300

    hyss

--- argpro/src/app/app.module.ts
+++ argpro/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -21,11 +22,12 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
+    HysComponent,
  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
